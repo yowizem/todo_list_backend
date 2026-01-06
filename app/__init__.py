6782e1bfa45c78892d39c68a25bb6c7d4d11f9f4
Kierby <dynamicmint21@gmail.com>
2026-01-07 07:17:25 +0800

    added app folder for main pyhton file

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+# to make an package(folder that is resuable in other folder/python file)
+# gawa lang ng folder tapos mag gawa ng file na ang name ay __init__.py
+
+# change mo rin yung directory for running fastapi application
+# uvicorn <folder name>.<file name>:app -- reload
+
+# default port for psql is 5432
+
+# timestamp is 240
+
+# unique contraint for column in psql
---
+++ app/main.py
@@ -0,0 +1,114 @@
+from fastapi import FastAPI, Response, status, HTTPException
+from typing import Optional
+from pydantic import BaseModel
+from datetime import date
+import random
+
+# documentation by me. the imported libraries is 
+app = FastAPI()
+
+# this is a model, kung baga ito ang susundan ng pag popost(blueprint)
+class Post(BaseModel):
+    todo_id: int
+    todo: str
+    content: str
+    is_done: bool = False
+    is_collaborative: Optional[bool]
+    created_at: str
+    updated_at: str
+
+# static data for testing(localstorage)
+todos = [{
+    "todo_id": 1,
+    "todo": "Upskill",
+    "content": "learning tobe the best",
+    "is_done": False,
+    "is_collaborative": None,
+    "created_at": "2026-01-07",
+    "updated_at": "2026-01-07"
+},{
+    "todo_id": 2,
+    "todo": "Being the best",
+    "content": "learning tobe the best",
+    "is_done": False,
+    "is_collaborative": None,
+    "created_at": "2026-01-07",
+    "updated_at": "2026-01-07"
+}]
+
+# function for a specific use
+def get_todo_id(id: int):
+    for todo in todos:
+        if todo["todo_id"] == id:
+            return todo
+    
+def find_todo_index(id: int):
+    for i, todo in enumerate(todos):
+        if todo["id"] == id:
+            return i
+        
+def date_now():
+    today = date.today()
+    return str(today)
+
+def id_generator():
+    id = random.randrange(0, 999999)
+    return int(id)
+
+# ito na yung pinaka routing lang(URI type shi)
+
+# getting all todos
+@app.get("/todos", status_code=status.HTTP_200_OK)
+def get_all_todos():
+    print(type(date_now()), type(id_generator()))
+    return{"todos": todos}
+
+# getting a specific todo by searching todo_id
+@app.get("/todos/{id}") 
+def get_todos_byId(id: int, response: Response):
+
+    my_todo = get_todo_id(id)
+
+    if not my_todo:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"The id: {id} is not found in the database!")
+    
+    # print(my_todo) -> para lang to sa debugging purposes
+    return{"todo": my_todo}
+
+# posting a new todo, basically mag add ka lang ng sarili mong todo
+@app.post("/todos", status_code=status.HTTP_201_CREATED)
+def create_todo(post: Post):
+    
+    todo = post.model_dump()
+
+    todo["todo_id"] = id_generator()
+    todo["created_at"] = date_now()
+    todo["updated_at"] = date_now()
+
+    todos.append(todo)
+    
+    return{"todos": todo}
+
+@app.put("/todos/{id}")
+def update_todo(id: int, up_todo: Post):
+
+    update = get_todo_id(id)
+
+    update["title"] = up_todo.title
+    update["content"] = up_todo.content
+
+    return up_todo
+
+@app.delete("/todos/{id}")
+def delete_todo(id: int):
+
+    delete_id = find_todo_index(id)
+
+    if delete_id == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"the id {id} is not in the database")
+
+    todos.pop(delete_id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
